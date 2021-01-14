687d00714adbd70d5f984dfce2214d72639d814e
LEONARDO DUPRATES <leoduprates@ext.worten.pt>
2021-01-14 15:38:34 +0000

    Renamed the parameter ByPassProxy to ByPassMock.

--- internal/server/dispatcher.go
+++ internal/server/dispatcher.go
@@ -140,16 +140,16 @@ func getProxyResponse(request *mock.Request, definition *mock.Definition) *mock.
 func (di *Dispatcher) getMatchingResult(request *mock.Request) (*mock.Definition, *match.Transaction) {
 	response := &mock.Response{}
 	mock, result := di.Resolver.Resolve(request)
-	var byPassProxy = false
+	var byPassMock = false
 
 	log.Printf("Definition match found: %s. Name : %s\n", strconv.FormatBool(result.Found), mock.URI)
 
-	if len(request.HttpHeaders.Headers["Bypassproxy"]) > 0 {
-		byPassProxy, _ = strconv.ParseBool(request.HttpHeaders.Headers["Bypassproxy"][0])
+	if len(request.HttpHeaders.Headers["Bypassmock"]) > 0 {
+		byPassMock, _ = strconv.ParseBool(request.HttpHeaders.Headers["Bypassmock"][0])
 	}
 
 	if result.Found {
-		if byPassProxy && len(mock.Control.ProxyBaseURL) > 0 {
+		if byPassMock && len(mock.Control.ProxyBaseURL) > 0 {
 			statistics.TrackProxyFeature()
 			response = getProxyResponse(request, mock)
 		} else {
